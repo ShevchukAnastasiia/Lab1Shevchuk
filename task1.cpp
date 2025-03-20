f0b30543aba94293d10a0693d1f54155120ebee1
ShevchukAnastasiia <shevchuk.anastasiia@chnu.edu.ua>
2025-03-20 10:30:08 +0200

    Update task1.cpp

--- task1.cpp
+++ task1.cpp
@@ -1,1 +1,33 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Hello, World!" << endl;
+
+    double num1, num2;
+    char op;
+
+    cout << "Введіть перше число: ";
+    cin >> num1;
+    cout << "Введіть оператор (+, -, *, /): ";
+    cin >> op;
+    cout << "Введіть друге число: ";
+    cin >> num2;
+
+    switch (op) {
+        case '+': cout << "Результат: " << num1 + num2 << endl; break;
+        case '-': cout << "Результат: " << num1 - num2 << endl; break;
+        case '*': cout << "Результат: " << num1 * num2 << endl; break;
+        case '/': 
+            if (num2 != 0)
+                cout << "Результат: " << num1 / num2 << endl;
+            else
+                cout << "Помилка: ділення на нуль!" << endl;
+            break;
+        default: cout << "Невідомий оператор!" << endl;
+    }
+
+    return 0;
+}
 
